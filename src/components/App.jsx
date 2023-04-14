c6fe4147ef76beb61a80537cae6d9083e0b6e97e
nasir <nasirdaneshiar@gmail.com>
2023-04-14 18:30:20 +0330

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import InputArea from "./InputArea";
+import ToDoItem from "./ToDoItem";
+
+
+function App() {
+  
+  const [items, setItems] = useState([]);
+
+  
+
+  function addItem(text) {
+    setItems(prevItems => {
+      return [...prevItems, text];
+    });
+    
+  }
+
+  function deleteItem(id){
+     setItems(prevItems =>{
+      return prevItems.filter((item,index)=>{
+        return index!==id;
+      })
+     })
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea AddItem ={addItem} />
+      <div>
+        <ul>
+          {items.map((todoItem,index) => (
+            <ToDoItem
+            key ={index}
+            id = {index}
+            text= {todoItem}
+            onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div> 
+    </div>
+  );
+
+}
+
+export default App;
